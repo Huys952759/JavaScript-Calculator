872cad1be60835ed6e8975c89ff650094e6ce5c9
Yaosong Hu <yaosonghu@Yaosongs-MacBook-Pro.local>
2020-05-15 15:38:36 +0800

    Updates

---
+++ precache-manifest.54422fa303281efcf0f9e88a68e0a730.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95151a6bf8170be0fe3bb69c6433bc35",
+    "url": "/JavaScript-Calculator/index.html"
+  },
+  {
+    "revision": "07c2fb386ac142f28c2f",
+    "url": "/JavaScript-Calculator/static/css/main.0fa6bf0b.chunk.css"
+  },
+  {
+    "revision": "582ae6ff9bc667ec908e",
+    "url": "/JavaScript-Calculator/static/js/2.70a9f01e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JavaScript-Calculator/static/js/2.70a9f01e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07c2fb386ac142f28c2f",
+    "url": "/JavaScript-Calculator/static/js/main.d6fb5722.chunk.js"
+  },
+  {
+    "revision": "9d80b13a91f8eff84582",
+    "url": "/JavaScript-Calculator/static/js/runtime-main.406aae28.js"
+  }
+]);
